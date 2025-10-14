8beccbb3a03a0435204cc17c31499771d0616992
Luca5Eckert <ecklukatv@gmail.com>
2025-10-14 19:13:27 -0300

    #11 feat: adiciona entidade de usuário

---
+++ src/main/java/com/api/blog/infrascturure/persistense/user/converter/EmailConverter.java
@@ -0,0 +1,20 @@
+package com.api.blog.infrascturure.persistense.user.converter;
+
+import com.api.blog.module.user.domain.vo.Email;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true)
+public class EmailConverter implements AttributeConverter<Email, String> {
+
+    @Override
+    public String convertToDatabaseColumn(Email email) {
+        return email != null ? email.address() : null;
+    }
+
+    @Override
+    public Email convertToEntityAttribute(String dbData) {
+        return dbData != null ? new Email(dbData) : null;
+    }
+
+}
---
+++ src/main/java/com/api/blog/infrascturure/persistense/user/converter/NameConverter.java
@@ -0,0 +1,19 @@
+package com.api.blog.infrascturure.persistense.user.converter;
+
+import com.api.blog.module.user.domain.vo.Name;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true) 
+public class NameConverter implements AttributeConverter<Name, String> {
+
+    @Override
+    public String convertToDatabaseColumn(Name name) {
+        return name != null ? name.value() : null;
+    }
+    @Override
+    public Name convertToEntityAttribute(String dbData) {
+        return dbData != null ? new Name(dbData) : null;
+    }
+
+}
