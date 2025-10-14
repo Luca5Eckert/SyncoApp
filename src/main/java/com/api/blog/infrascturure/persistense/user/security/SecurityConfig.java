05a9340776be09ec5b2d73a7b8c5358206f0797b
Luca5Eckert <ecklukatv@gmail.com>
2025-10-14 20:09:19 -0300

    #3 chore: adiciona configuração de security para teste

--- src/main/java/com/api/blog/infrascturure/persistense/user/security/SecurityConfig.java
+++ src/main/java/com/api/blog/infrascturure/persistense/user/security/SecurityConfig.java
@@ -2,10 +2,15 @@ package com.api.blog.infrascturure.persistense.user.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -13,4 +18,17 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // 1. Desabilita a proteção CSRF, pois não é comum em APIs stateless
+                .csrf(AbstractHttpConfigurer::disable)
+
+                // 2. Define as regras de autorização para as requisições
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
 }
