a081dac9a4cc6675edd40d56f2a3c259c4ffc617
Luca5Eckert <ecklukatv@gmail.com>
2025-10-15 20:37:01 -0300

    #18 feat: adiciona classe UserDetailsImpl

---
+++ src/main/java/com/api/blog/infrastructure/security/user_details/UserDetailsImpl.java
@@ -0,0 +1,43 @@
+package com.api.blog.infrastructure.security.user_details;
+
+import com.api.blog.module.user.domain.enumerator.RoleUser;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private final long id;
+    private final String email;
+    private final String password;
+    private final RoleUser roleUser;
+
+    public UserDetailsImpl(long id, String email, String password, RoleUser roleUser) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.roleUser = roleUser;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(roleUser);
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    public long getId(){
+        return id;
+    }
+
+}
