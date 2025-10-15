6c18ec8ba1642449c3fac1f52ed2259279a485ae
Luca5Eckert <ecklukatv@gmail.com>
2025-10-15 15:52:24 -0300

    #5 feat: adiciona documentação do método para executar o caso de uso de editar usuário

--- src/main/java/com/api/blog/module/user/domain/use_case/UserEditUseCase.java
+++ src/main/java/com/api/blog/module/user/domain/use_case/UserEditUseCase.java
@@ -18,6 +18,18 @@ public class UserEditUseCase {
         this.userRepository = userRepository;
     }
 
+    /**
+     * This Method is responsible for executing the
+     * use case of edit a user
+     *
+     * <p> In this use case will happen the validation if user
+     * can edit another, or himself, user. After the validation
+     * the user will be edit and save in Database.</p>
+     *
+     * @param userEditRequest Data of the edit
+     * @param idUserAutenticated id of the user who will edit
+     * @return The user edited
+     */
     public UserEntity execute(UserEditRequest userEditRequest, long idUserAutenticated) {
         UserEntity userAuthenticated = userRepository.findById(idUserAutenticated).orElseThrow(() -> new UserNotFoundException(idUserAutenticated));
         UserEntity userEdit = userRepository.findById(userEditRequest.id()).orElseThrow( () -> new UserNotFoundException(userEditRequest.id()));
