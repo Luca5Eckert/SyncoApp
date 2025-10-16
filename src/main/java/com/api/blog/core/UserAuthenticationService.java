64df379c08f1fdcd97d0ed5154857ec98b0cf213
Luca5Eckert <ecklukatv@gmail.com>
2025-10-15 21:21:43 -0300

    #8 feat: adiciona implementacao da interface UserAuthenticationService

--- src/main/java/com/api/blog/core/UserAuthenticationService.java
+++ src/main/java/com/api/blog/core/UserAuthenticationService.java
@@ -1,7 +1,11 @@
 package com.api.blog.core;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
 public interface UserAuthenticationService {
 
     long getAuthenticatedUserId();
+
+    UserDetails getUserDetails();
 
 }
--- src/main/java/com/api/blog/infrastructure/service/UserAuthenticationServiceImpl.java
+++ src/main/java/com/api/blog/infrastructure/service/UserAuthenticationServiceImpl.java
@@ -1,12 +1,24 @@
 package com.api.blog.infrastructure.service;
 
 import com.api.blog.core.UserAuthenticationService;
+import com.api.blog.infrastructure.security.user_details.UserDetailsImpl;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserAuthenticationServiceImpl implements UserAuthenticationService {
+
     @Override
     public long getAuthenticatedUserId() {
-        return 2;
+        var userDetails = (UserDetailsImpl) getUserDetails();
+
+        return userDetails.getId();
     }
+
+    @Override
+    public UserDetails getUserDetails() {
+        return (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+
 }
