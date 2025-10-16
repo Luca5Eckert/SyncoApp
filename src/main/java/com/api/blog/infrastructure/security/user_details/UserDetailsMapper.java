c0babd20f9301142d7edcc443b7907afc86ba946
Luca5Eckert <ecklukatv@gmail.com>
2025-10-15 21:02:55 -0300

    #18 feat: adiciona anotação para beans

--- src/main/java/com/api/blog/infrastructure/security/user_details/UserDetailsMapper.java
+++ src/main/java/com/api/blog/infrastructure/security/user_details/UserDetailsMapper.java
@@ -2,7 +2,9 @@ package com.api.blog.infrastructure.security.user_details;
 
 import com.api.blog.module.user.domain.UserEntity;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserDetailsMapper {
 
     public UserDetails toEntity(UserEntity user) {
