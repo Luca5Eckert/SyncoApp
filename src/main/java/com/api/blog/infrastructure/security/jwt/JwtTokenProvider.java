73eaa52abb902def06682736ab784c9505bd0f5d
Luca5Eckert <ecklukatv@gmail.com>
2025-10-15 21:02:27 -0300

    #22 feat: adiciona classe JwtTokenProvider

---
+++ src/main/java/com/api/blog/infrastructure/security/jwt/JwtTokenProvider.java
@@ -0,0 +1,71 @@
+package com.api.blog.infrastructure.security.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.security.Key;
+import java.util.UUID;
+
+@Component
+public class JwtTokenProvider {
+
+    private final Key key;
+    private final long validityInMilliseconds;
+
+    public JwtTokenProvider(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.token.validity}") long validityInMilliseconds
+    ) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+        this.validityInMilliseconds = validityInMilliseconds;
+    }
+
+    /**
+     * Gera um token JWT para um usuário, incluindo o ID.
+     * @param email O email de usuário para o qual o token será gerado.
+     * @return O token JWT como uma string.
+     */
+    public String generateToken(String email) {
+        Claims claims = Jwts.claims().setSubject(email);
+        claims.put("email", email);
+
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expiration)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    /**
+     * Valida um token JWT.
+     * @param token O token JWT a ser validado.
+     * @return true se o token for válido, false caso contrário.
+     */
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    /**
+     * Obtém o ID do usuário de um token JWT.
+     * @param token O token JWT.
+     * @return O ID do usuário.
+     */
+    public String getUserEmail(String token) {
+        Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+        return (String) claims.get("email");
+    }
+}
