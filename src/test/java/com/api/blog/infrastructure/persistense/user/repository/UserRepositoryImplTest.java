32b4f49cea0d0b6a1f1a1d65334f527bbfbbb3bf
Luca5Eckert <ecklukatv@gmail.com>
2025-10-21 14:11:39 -0300

    #23 feat: adiciona cobertura de testes para PasswordValidatorImpl

---
+++ src/test/java/com/api/blog/infrastructure/persistense/user/repository/UserRepositoryImplTest.java
@@ -0,0 +1,57 @@
+package com.api.blog.infrastructure.persistense.user.repository;
+
+import com.api.blog.module.user.domain.UserEntity;
+import com.api.blog.module.user.domain.enumerator.RoleUser;
+import com.api.blog.module.user.domain.port.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class UserRepositoryImplTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private long id;
+    private UserEntity user;
+
+    @BeforeEach
+    public void setup(){
+        id = 1L;
+        user = new UserEntity(id, null, null, null, RoleUser.ADMIN);
+    }
+
+    @Test
+    void shouldSave() {
+    }
+
+    @Test
+    void shouldReturnTrueWhenExistsByEmail() {
+    }
+
+    @Test
+    void shouldReturnUserWhenFoundById() {
+    }
+
+    @Test
+    void shouldReturnTrueWhenExistsById() {
+    }
+
+    @Test
+    void shouldDeleteWhenFoundById() {
+    }
+
+    @Test
+    void shouldReturnAllUsersWhenFound() {
+    }
+
+    @Test
+    void shouldReturnUserWhenFoundByEmail() {
+    }
+
+}
