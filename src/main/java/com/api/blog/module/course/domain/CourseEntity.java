d5e28accaa486556994925a65dc0e0d5642a21d7
Luca5Eckert <ecklukatv@gmail.com>
2025-10-30 21:15:08 -0300

    feat: adiciona entidade de curso

--- src/main/java/com/api/blog/module/course/domain/CourseEntity.java
+++ src/main/java/com/api/blog/module/course/domain/CourseEntity.java
@@ -1,4 +1,45 @@
 package com.api.blog.module.course.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class CourseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    private String acronym;
+
+    public CourseEntity(String acronym, String name, long id) {
+        this.acronym = acronym;
+        this.name = name;
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAcronym() {
+        return acronym;
+    }
+
+    public void setAcronym(String acronym) {
+        this.acronym = acronym;
+    }
+
 }
