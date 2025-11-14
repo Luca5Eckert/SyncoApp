71536b5e5ffcb92edd18812b3c784247f3e38040
Luca5Eckert <ecklukatv@gmail.com>
2025-11-13 21:05:59 -0300

    fix: adiciona tratamento de erro para autenticação

--- src/main/java/com/api/synco/infrastructure/exception/GlobalExceptionHandler.java
+++ src/main/java/com/api/synco/infrastructure/exception/GlobalExceptionHandler.java
@@ -5,10 +5,12 @@ import com.api.synco.infrastructure.exception.token.TokenException;
 import com.api.synco.module.authentication.domain.exception.AuthenticationException;
 import com.api.synco.module.course.domain.exception.CourseDomainException;
 import com.api.synco.module.user.domain.exception.UserDomainException;
+import com.api.synco.module.user.domain.exception.UserNotFoundDomainException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -22,6 +24,20 @@ public class GlobalExceptionHandler {
         String path = httpServletRequest.getRequestURI();
 
         return ResponseEntity.badRequest().body(CustomApiResponse.error(HttpStatus.BAD_REQUEST.value(), "USER_EXCEPTION", e.getMessage(), path));
+    }
+
+    @ExceptionHandler(InternalAuthenticationServiceException.class)
+    public ResponseEntity<CustomApiResponse<?>> handlerInternalAuthenticationException(InternalAuthenticationServiceException e, HttpServletRequest httpServletRequest) {
+        String path = httpServletRequest.getRequestURI();
+
+        HttpStatus status = HttpStatus.UNAUTHORIZED;
+
+        return ResponseEntity.status(status).body(CustomApiResponse.error(
+                status.value(),
+                "AUTHENTICATION_SERVICE_ERROR",
+                "Authentication failed: " + e.getMessage(),
+                path)
+        );
     }
 
     @ExceptionHandler(CourseDomainException.class)
